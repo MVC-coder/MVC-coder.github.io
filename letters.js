ef2afa1aeba077c19b05a07c5e6f913b70ff1ca2
u0036408 <maarten.vancasteren@kuleuven.be>
2025-01-19 11:56:38 +0000

    temp commit letters

---
+++ letters.js
@@ -0,0 +1,101 @@
+var allLetters = {
+    ik: {
+            letters: ["i","k"]
+        },
+    mol:{
+            letters: ["m","o","l"]
+        },
+    beer: {
+            letters: ["b","ee","r"]
+        },
+    an: {
+            letters: ["a","n"]
+        },
+    tom: {
+            letters: ["t"]
+        },
+    pen: {
+            letters: ["p","e"]
+        },
+    jas: {
+            letters: ["j","s"]
+        },
+    mus: {
+            letters: ["u"]
+        },
+    vis: {
+            letters:["v"]
+        },
+    poes: {
+            letters: ["oe"]
+        },
+    haan: {
+            letters: ["h","aa"]
+        },
+    boom: {
+            letters: ["oo"]
+        },
+    weg: {
+            letters: ["w","g"]
+        },
+    muur: {  
+            letters: ["uu"]
+        },
+    deur: {
+            letters: ["d","eu"]
+        },
+    ijs: {
+            letters: ["ij"]
+        },
+    school: {
+            letters: ["sch"]
+        },
+    vier: {
+            letters: ["ie"]
+        },
+    touw: {
+            letters: ["ou"]
+        },
+    duif: {
+            letters: ["ui","f"]
+        },
+    zon: {
+            letters: ["z"]
+        },
+    haai: {
+            letters: ["aai"]
+        },
+    kooi: {
+            letters:["ooi"]
+        },
+    roei: {
+            letters:["oei"]
+        },
+    pauw: {
+            letters:["au"]
+        },
+    kei: {
+            letters:["ei"]
+        },
+    carnaval: {
+            letters: ["c"]
+        },
+    bank: {
+            letters: ["nk"]
+        },
+    wang: {
+            letters:["ng"]
+        },
+    leeuw: {
+            letters:["eeuw"]
+        },
+    nieuw: {
+            letters:["ieuw"]
+        },
+    lach: {
+            letters:["ch"]
+        },
+    lucht: {
+            letters:["cht"]
+        }
+}
