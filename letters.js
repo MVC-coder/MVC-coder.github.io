3479d5ab84ad6727276a11487f68f25459375929
MVC-coder <maarten_van_casteren@hotmail.com>
2026-09-07 17:08:25 +0200

    changed allLettersObject

--- letters.js
+++ letters.js
@@ -1,101 +1,428 @@
 var allLetters = {
-    ik: {
-            letters: ["i","k"]
-        },
-    mol:{
-            letters: ["m","o","l"]
-        },
-    beer: {
-            letters: ["b","ee","r"]
-        },
-    an: {
-            letters: ["a","n"]
-        },
-    tom: {
-            letters: ["t"]
-        },
-    pen: {
-            letters: ["p","e"]
-        },
-    jas: {
-            letters: ["j","s"]
-        },
-    mus: {
-            letters: ["u"]
-        },
-    vis: {
-            letters:["v"]
-        },
-    poes: {
-            letters: ["oe"]
-        },
-    haan: {
-            letters: ["h","aa"]
-        },
-    boom: {
-            letters: ["oo"]
-        },
-    weg: {
-            letters: ["w","g"]
-        },
-    muur: {  
-            letters: ["uu"]
-        },
-    deur: {
-            letters: ["d","eu"]
-        },
-    ijs: {
-            letters: ["ij"]
-        },
-    school: {
-            letters: ["sch"]
-        },
-    vier: {
-            letters: ["ie"]
-        },
-    touw: {
-            letters: ["ou"]
-        },
-    duif: {
-            letters: ["ui","f"]
-        },
-    zon: {
-            letters: ["z"]
-        },
-    haai: {
-            letters: ["aai"]
-        },
-    kooi: {
-            letters:["ooi"]
-        },
-    roei: {
-            letters:["oei"]
-        },
-    pauw: {
-            letters:["au"]
-        },
-    kei: {
-            letters:["ei"]
-        },
-    carnaval: {
-            letters: ["c"]
-        },
-    bank: {
-            letters: ["nk"]
-        },
-    wang: {
-            letters:["ng"]
-        },
-    leeuw: {
-            letters:["eeuw"]
-        },
-    nieuw: {
-            letters:["ieuw"]
-        },
-    lach: {
-            letters:["ch"]
-        },
-    lucht: {
-            letters:["cht"]
+    i: {
+        letterSteunAI: [{
+            image: "i.png",
+            name: "inktvis"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "ik"
         }
+    },
+    k: {
+        letterSteunAI: [{
+            image: "k.png",
+            name: "krokodil"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "ik"
+        }
+    },
+    m: {
+        letterSteunAI: [{
+            image: "m-manta.png",
+            name: "manta"
+        },{
+            image:"m-muur.png",
+            name: "muur"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "mol"
+        }
+    },
+    o: {
+        letterSteunAI: [{
+            image: "o-orkaan.png",
+            name: "orkaan"
+        },{
+            image: "o-orkest.png",
+            name: "orkest"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "mol"
+        }
+    },
+    l: {
+        letterSteunAI: [{
+            image: "l.png",
+            name: "ladder"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "mol"
+        }
+    },
+    b: {
+        letterSteunAI: [{
+            image: "b.png",
+            name: "beer"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "beer"
+        }
+    },
+    ee: {
+        letterSteunAI: [{
+            image: "ee.png",
+            name: "eenhoorn"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "beer"
+        }
+    },
+    r: {
+        letterSteunAI: [{
+            image: "r.png",
+            name: "ridder"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "beer"
+        }
+    },
+    a: {
+        letterSteunAI: [{
+            image: "a.png",
+            name: "appel"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "an"
+        }
+    },
+    n: {
+        letterSteunAI: [{
+            image: "n.png",
+            name: "nietje"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "an"
+        }
+    },
+    t: {
+        letterSteunAI: [{
+            image: "t.png",
+            name: "tak"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "tom"
+        }
+    },
+    p: {
+        letterSteunAI: [{
+            image: "p-paard1.png",
+            name: "paard"
+        },{
+            image: "p-paard2.png",
+            name: "paard"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "pen"
+        }
+    },
+    e: {
+        letterSteunAI: [{
+            image: "e.png",
+            name: "emmer"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "pen"
+        }
+    },
+    j: {
+        letterSteunAI: [{
+            image: "j.png",
+            name: "jojo"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "jas"
+        }
+    },
+    s: {
+        letterSteunAI: [{
+            image: "s.png",
+            name: "slang"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "jas"
+        }
+    },
+    u: {
+        letterSteunAI: [{
+            image: "u.png",
+            name: "put"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "mus"
+        }
+    },
+    v: {
+        letterSteunAI: [{
+            image: "v.png",
+            name: "vogel"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "vis"
+        }
+    },
+    oe: {
+        letterSteunAI: [{
+            image: "oe.png",
+            name: "oehoe"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "poes"
+        }
+    },
+    h: {
+        letterSteunAI: [{
+            image: "h.png",
+            name: "haan"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "haan"
+        }
+    },
+    aa: {
+        letterSteunAI: [{
+            image: "aa.png",
+            name: "aap"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "haan"
+        }
+    },
+   oo: {
+        letterSteunAI: [{
+            image: "oo.png",
+            name: "oog"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "boom"
+        }
+    },
+    w: {
+        letterSteunAI: [{
+            image: "w.png",
+            name: "was"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "weg"
+        }
+    },
+    g: {
+        letterSteunAI: [{
+            image: "g.png",
+            name: "goudvis"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "weg"
+        }
+    },
+    uu: {
+        letterSteunAI: [{
+            image: "uu.png",
+            name: "vuur"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "muur"
+        }
+    },
+    d: {
+        letterSteunAI: [{
+            image: "d.png",
+            name: "dino"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "deur"
+        }
+    },
+    eu: {
+        letterSteunAI: [{
+            image: "eu.png",
+            name: "neushoorn"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "deur"
+        }
+    },
+    ij: {
+        letterSteunAI: [{
+            image: "ij.png",
+            name: "ijs"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "ijs"
+        }
+    },
+    sch: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "school"
+        }
+    },
+    ie: {
+        letterSteunAI: [{
+            image: "ie.png",
+            name: "gieter"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "vier"
+        }
+    },
+    ou: {
+        letterSteunAI: [{
+            image: "ou.png",
+            name: "touw"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "touw"
+        }
+    },
+    ui: {
+        letterSteunAI: [{
+            image: "ui.png",
+            name: "uil"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "duif"
+        }
+    },
+    f: {
+        letterSteunAI: [{
+            image: "f.png",
+            name: "flamingo"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "duif"
+        }
+    },
+    z: {
+        letterSteunAI: [{
+            image: "z.png",
+            name: "zwaan"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "zon"
+        }
+    },
+    aai: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "haai"
+        }
+    },
+    ooi: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "kooi"
+        }
+    },
+    oei: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "roei"
+        }
+    },
+    au: {
+        letterSteunAI: [{
+            image: "au.png",
+            name: "auto"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "pauw"
+        }
+    },
+    ei: {
+        letterSteunAI: [{
+            image: "ei.png",
+            name: "ei"
+        }],
+        molEnBeer: {
+            image: "",
+            name: "kei"
+        }
+    },
+    c: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "carnaval"
+        }
+    },
+    nk: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "bank"
+        }
+    },
+    ng: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "wang"
+        }
+    },
+    eeuw: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "leeuw"
+        }
+    },
+    ieuw: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "nieuw"
+        }
+    },
+    ch: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "lach"
+        }
+    },
+    cht: {
+        letterSteunAI: [],
+        molEnBeer: {
+            image: "",
+            name: "lucht"
+        }
+    }
 }
